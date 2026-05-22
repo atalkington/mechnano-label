88b45acc24ffce293c3dfe17f03a765a6b6a0e14
atalkington <atalkington@mechnano.com>
2026-05-22 15:58:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def build_html(name, sku, net_weight, lot_number, mfg_date, coo):
 
         .diag2 {{
             position:absolute;
-            top:-100px;
+            top:-500px;
             left:-300px;
             width:3500px;
             height:600px;
